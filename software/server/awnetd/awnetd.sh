412786a7d70b54009ccd9a16ce2eb42198ce5779
Steve Marple <stevemarple@googlemail.com>
2013-02-13 21:25:09 +0000

    Adding Required-Start/Required-Stop.

--- software/server/awnetd/awnetd.sh
+++ software/server/awnetd/awnetd.sh
@@ -2,6 +2,8 @@
 
 ### BEGIN INIT INFO
 # Provides:          awnetd
+# Required-Start:
+# Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Description:       AuroraWatchNet data collection
