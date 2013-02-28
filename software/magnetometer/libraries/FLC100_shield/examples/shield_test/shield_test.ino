9fc2a2ca2aa4dadbfe878c1527a0194cb724e96b
Steve Marple <stevemarple@googlemail.com>
2013-02-28 23:57:46 +0000

    Modifications for 3-axis sensor head.

--- software/magnetometer/libraries/FLC100_shield/examples/shield_test/shield_test.ino
+++ software/magnetometer/libraries/FLC100_shield/examples/shield_test/shield_test.ino
@@ -7,13 +7,15 @@
 #include <FLC100_shield.h>
 
 
-MCP342x adc;
 // List of possible addresses for the ADC; 0x68 and 0x6F omitted since
 // they clash with the address used by the DS1307 and similar
 // real-time clocks and the MCP7941x.
-const uint8_t adcAddressList[] = {0x69, 0x6A, 0x6B,
-				  0x6C, 0x6D, 0x6E};
-bool haveMCP342x = false;
+const uint8_t numAddresses = 6;
+const uint8_t adcAddressList[numAddresses] = {0x6E, 0x6A, 0x6C,  // Normal X, Y, Z
+					      0x69, 0x6B, 0x6D}; // Other options
+bool haveMCP342x[numAddresses];
+MCP342x adc[numAddresses];
+uint8_t found = 0;
 
 uint8_t xrfSleepState = 0;
 uint8_t xrfResetState = 1;
@@ -40,14 +42,27 @@ void setup(void)
   digitalWrite(FLC100_POWER, adcPowerState);
 
   // Reset all MCP342x devices
-  adc.generalCallReset();
-  haveMCP342x = adc.autoprobe(adcAddressList, sizeof(adcAddressList));
-  if (haveMCP342x) {
-    Serial.print("Detected ADC at 0x");
-    Serial.println(adc.getAddress(), HEX);
-  }
-  else
-    Serial.println("Could not detect MCP7941x");
+  MCP342x::generalCallReset();
+  delay(100);
+  for (uint8_t i = 0; i < numAddresses; ++i)
+    haveMCP342x[i] = adc[i].autoprobe(&adcAddressList[i], 1);
+
+  // Print out found and missing, group
+  for (uint8_t i = 0; i < numAddresses; ++i)
+    if (haveMCP342x[i]) {
+      Serial.print("Detected ADC at 0x");
+      Serial.println(adcAddressList[i], HEX);
+      ++found;
+    }
+
+  for (uint8_t i = 0; i < numAddresses; ++i)
+    if (!haveMCP342x[i]) {
+      Serial.print("Did not detect ADC at 0x");
+      Serial.println(adcAddressList[i], HEX);
+    }
+
+
+
 }
 
 
@@ -77,16 +92,16 @@ void loop(void)
 	  digitalWrite(XRF_RESET, xrfResetState);
 	}
 	else if (strcmp_P(buffer, PSTR("gain1")) == 0) {
-	  gain = adc.gain1;
+	  gain = MCP342x::gain1;
 	}
 	else if (strcmp_P(buffer, PSTR("gain2")) == 0) {
-	  gain = adc.gain2;
+	  gain = MCP342x::gain2;
 	}
 	else if (strcmp_P(buffer, PSTR("gain4")) == 0) {
-	  gain = adc.gain4;
+	  gain = MCP342x::gain4;
 	}
 	else if (strcmp_P(buffer, PSTR("gain8")) == 0) {
-	  gain = adc.gain8;
+	  gain = MCP342x::gain8;
 	}
 	else if (strcmp_P(buffer, PSTR("+++")) == 0) {
 	  Serial1.print("+++");
@@ -128,17 +143,29 @@ void loop(void)
     Serial.print("Vin: ");
     Serial.print((3.3 * analogRead(BATTERY_ADC)) / 1024);
     Serial.println(" V");
-    if (haveMCP342x && adcPowerState) {
-      // Sample channel 0
-      int32_t x;
-      MCP342x::Config status;
-      adc.convertAndRead(MCP342x::channel1, MCP342x::oneShot,
-			 MCP342x::resolution18, gain,
-			 300000UL, x, status);
-      Serial.print("X: ");
-      Serial.println(x);
+    if (adcPowerState) {
+      float total = 0.0;
+      for (uint8_t i = 0; i < numAddresses; ++i) {
+	if (haveMCP342x[i]) {
+	  // Sample channel 0
+	  int32_t x;
+	  MCP342x::Config status;
+	  adc[i].convertAndRead(MCP342x::channel1, MCP342x::oneShot,
+				MCP342x::resolution18, gain,
+				300000UL, x, status);
+	  Serial.print("ADC 0x");
+	  Serial.print(adcAddressList[i], HEX);
+	  Serial.print(": ");
+	  Serial.println(x);
+	  total += (double(x) * double(x));
+	}
+      }
+      if (found == 3) {
+	Serial.print("Total: ");
+	Serial.println(sqrt(total));
+      }
     }
-
+    
   }
   
   
