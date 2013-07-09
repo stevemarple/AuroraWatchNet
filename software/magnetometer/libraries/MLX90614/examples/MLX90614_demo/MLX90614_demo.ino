20fbc9e629c2b3626a8cc8f060c5e26aa722abea
Steve Marple <stevemarple@googlemail.com>
2013-07-09 23:56:08 +0100

    Update to work with new MLX90614 library and state machine.

--- software/magnetometer/libraries/MLX90614/examples/MLX90614_demo/MLX90614_demo.ino
+++ software/magnetometer/libraries/MLX90614/examples/MLX90614_demo/MLX90614_demo.ino
@@ -1,48 +1,51 @@
-#include <mysofti2cmaster.h>
-#include <MLX90614.h>
 #include <AsyncDelay.h>
+#include <SoftWire.h>
+#include <MLX90614.h>
 #include <DisableJTAG.h>
 
 MLX90614 mlx90614;
 AsyncDelay samplingInterval;
 
-inline float convertToDegC(uint16_t data)
+inline float convertToDegC(int16_t data)
 {
-  return (float(MLX90614::convertToCentiK(data)) / 100) - 273.15;
+  return (data / 100.0);
 }
 
 void setup(void)
 {
   Serial.begin(9600);
+  Serial.println("MLX90614_demo");
 #ifdef JTD
   disableJTAG();
 #endif
   mlx90614.initialise();
-  samplingInterval.start(3000, AsyncDelay::MILLIS);
+  samplingInterval.start(1000, AsyncDelay::MILLIS);
 }
 
-
+bool printed = true;
 void loop(void)
 {
   if (samplingInterval.isExpired() && !mlx90614.isSampling()) {
     mlx90614.start();
+    printed = false;
     samplingInterval.repeat();
     Serial.println("Sampling started");
   }
 
   mlx90614.process();
 
-  if (mlx90614.isFinished() && !mlx90614.isPowerOff()) {
-    mlx90614.powerOff();
+  if (mlx90614.isFinished() && !printed) {
+    printed = true;
     // Print saved values
     Serial.print("Ambient: ");
-    Serial.println(convertToDegC(mlx90614.getAmbient()));
-    Serial.print("Object 1: ");
-    Serial.println(convertToDegC(mlx90614.getObject1()));
+    Serial.print(convertToDegC(mlx90614.getAmbient()));
+    Serial.print("    Object 1: ");
+    Serial.print(convertToDegC(mlx90614.getObject1()));
     if (mlx90614.isDualSensor()) {
       Serial.print("Object 2: ");
-      Serial.println(convertToDegC(mlx90614.getObject2()));
+      Serial.print(convertToDegC(mlx90614.getObject2()));
     }
+    Serial.println();
   }
   
 }
