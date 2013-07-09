bbc3798315b8288825fd72df8e98e71f02e903ca
Steve Marple <stevemarple@googlemail.com>
2013-07-09 23:58:51 +0100

    Functions to disable/enable JTAG.

---
+++ software/magnetometer/libraries/DisableJTAG/DisableJTAG.h
@@ -0,0 +1,15 @@
+#ifndef DISABLE_JTAG_H
+#define DISABLE_JTAG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void disableJTAG(void);
+void enableJTAG(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
