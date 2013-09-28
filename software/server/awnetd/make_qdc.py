2dab65de0af18845a5774c9064005444196efc11
Steve Marple <stevemarple@googlemail.com>
2013-09-28 21:58:16 +0100

    Modified to use same style of options as awnet_plot.py. Sets TZ to UTC.

--- software/server/awnetd/make_qdc.py
+++ software/server/awnetd/make_qdc.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import os.path
 import sys
+import time
 
 import numpy as np
 import matplotlib as mpl
@@ -20,8 +21,11 @@ import auroraplot.magdata
 import auroraplot.datasets.aurorawatchnet
 import auroraplot.datasets.samnet
 
+# Set timezone appropriately to get intended np.datetime64 behaviour.
+os.environ['TZ'] = 'UTC'
+time.tzset()
 
-# Check timezone set appropriately to get intended np.datetime64 behaviour.
+
 assert os.environ.get('TZ') == 'UTC', \
     'TZ environment variable must be set to UTC'
 
@@ -40,14 +44,14 @@ parser.add_argument('-e', '--end-time',
 parser.add_argument('-v', '--verbose', 
                     action='store_true', 
                     help='Be verbose')
-parser.add_argument('-n', '--network', 
-                    default='AURORAWATCHNET',
-                    help='Network',
-                    metavar='NETWORK')
-parser.add_argument('--site', 
+parser.add_argument('--sites', 
                     required=True,
-                    help='Site abbreviation',
-                    metavar='SITE')
+                    help='Whitespace-separated list of networks and/or ' + \
+                        'sites. Sites must be prefixed with their ' + \
+                        'network and separated by a "/". If a network is ' + \
+                        'listed without a site then all sites for that ' + \
+                        'network are processed.',
+                    metavar='"NETWORK1/SITE1 NETWORK2/SITE2 ..."')
 parser.add_argument('--archive',
                     help='Data archive name')
 parser.add_argument('--dry-run',
@@ -89,51 +93,70 @@ else:
 print('Start date: ' + str(start_time))
 print('End date: ' + str(end_time))
 
-network = args.network.upper()
-site = args.site.upper()
+# Get names of all networks and sites to be processed. Dictionary used
+# to avoid duplicates.
+network_site = {}
+for s in args.sites.upper().split():
+    n_s = s.split('/')
+    if len(n_s) == 1:
+        # Only network given, use all sites
+        for k in ap.networks[n_s[0]].keys():
+            network_site[n_s[0] + '/' + k] = (n_s[0], k)
+
+    elif len(n_s) == 2:
+        # Network and site given
+        network_site[s] = tuple(n_s)
+    else:
+        raise Exception('bad value for network/site (' + network_site)
+
 
 ap.verbose = args.verbose
     
 
-ax = None
-t1 = dt64.get_start_of_month(start_time)
-while t1 < end_time:
-    t2 = dt64.get_start_of_next_month(t1)
-    
+for network_uc, site_uc in network_site.values():
+    site_lc = site_uc.lower()
+    network_lc = network_uc.lower()
+
+    ax = None
+    t1 = dt64.get_start_of_month(start_time)
+    while t1 < end_time:
+        t2 = dt64.get_start_of_next_month(t1)
+
+        if args.plot:
+            mag_qdc = ap.magdata.load_qdc(network_uc, site_uc, t1)
+            if mag_qdc is not None:
+                lh = mag_qdc.plot(axes=ax)
+                for h in lh:
+                    h.set_label(dt64.strftime(t1, '%Y-%m-%d'))
+                ax = plt.gca()
+
+        else:
+            mag_data = ap.load_data(network_uc, site_uc, 'MagData', t1, t2)
+            if mag_data is not None:
+                mag_qdc = mag_data.make_qdc(smooth=True)
+
+                archive, ad = ap.get_archive_details(network_uc, site_uc, 
+                                                     'MagQDC', 
+                                                     archive=getattr(args, 
+                                                                     'archive'))
+                filename = dt64.strftime(t1, ad['path'])
+                p = os.path.dirname(filename)
+                if not os.path.isdir(p):
+                    os.makedirs(p)
+                if args.dry_run:
+                    print('Dry run, not saving QDC to ' + filename)
+                else:
+                    mag_qdc.savetxt(filename)
+
+        t1 = t2
+
     if args.plot:
-        mag_qdc = ap.magdata.load_qdc(network, site, t1)
-        if mag_qdc is not None:
-            lh = mag_qdc.plot(axes=ax)
-            for h in lh:
-                h.set_label(dt64.strftime(t1, '%Y-%m-%d'))
-            ax = plt.gca()
-
-    else:
-        mag_data = ap.load_data(network, site, 'MagData', t1, t2)
-        if mag_data is not None:
-            mag_qdc = mag_data.make_qdc(smooth=True)
-    
-            archive, ad = ap.get_archive_details(network, site, 'MagQDC', 
-                                                 archive=getattr(args, 
-                                                                 'archive'))
-            filename = dt64.strftime(t1, ad['path'])
-            p = os.path.dirname(filename)
-            if not os.path.isdir(p):
-                os.makedirs(p)
-            if args.dry_run:
-                print('Dry run, not saving QDC to ' + filename)
-            else:
-                mag_qdc.savetxt(filename)
-
-    t1 = t2
-
-if args.plot:
-    if hasattr(ax, '__iter__'):
-        for h in ax:
-            legh = h.legend(fancybox=True, loc='best')
+        if hasattr(ax, '__iter__'):
+            for h in ax:
+                legh = h.legend(fancybox=True, loc='best')
+                legh.get_frame().set_alpha(0.7)
+        else:
+            legh = ax.legend(fancybox=True, loc='best')
             legh.get_frame().set_alpha(0.7)
-    else:
-        legh = ax.legend(fancybox=True, loc='best')
-        legh.get_frame().set_alpha(0.7)
-    plt.show()
+        plt.show()
 
