7cbbd4769c8bfe5e85c73ca010965c0624c2eaac
Steve Marple <stevemarple@googlemail.com>
2014-07-21 22:16:15 +0100

    Adding a data-quality monitor to check the status of a switch. WIP.
    When a switch connected to CTS is pressed the a file is created, signalling to the
    AuroraWatchNet daemon that data quality may be impaired for some reason. Pressing
    the switch again toggles state and removes the file. This python script handles the
    daemon start/stop/restart and the actual work.
    
    Work in progress. Need to drop root priviledges.

---
+++ software/server/bin/awnetd_monitor.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+
+# Program to monitor a serial port to detect change of state in CTS,
+# indicating a push-switch has been pressed. Switch presses toggle the
+# existence or removal of a semaphore file. The serial control line
+# DTR is used to indicate the presence of the file, by lighting an LED
+# connected to it. Switch debouncing is implemented to confirm the
+# switch state. In the case of an error the LED is flashed.
+#
+# The purpose of this program in the AuroraWatchNet system is to allow
+# a non-technical use to flag when data quality may be suspect (for
+# instance, when grass-cutting at the site is about to commence). By
+# using a semaphore file to signal to the recording process it is easy
+# to change the data quality status remotely.
+
+import argparse
+import daemon
+import daemon.runner
+import logging
+import os
+import signal
+import re
+import sys
+import time
+from serial import Serial
+from fcntl import  ioctl
+from termios import (
+    TIOCMIWAIT,
+    TIOCM_RNG,
+    TIOCM_DSR,
+    TIOCM_CD,
+    TIOCM_CTS
+)
+
+if sys.version_info[0] >= 3:
+    import configparser
+    from configparser import SafeConfigParser
+else:
+    import ConfigParser
+    from ConfigParser import SafeConfigParser
+
+import aurorawatchnet as awn
+
+logger = logging.getLogger(__name__)
+
+
+class FtdiMonitor():
+    def __init__(self, progname, device=None, filename=None,
+                 pidfile_path=None, pidfile_timeout=None):
+        self.progname = progname
+        self.device = device
+        self.filename = filename
+        
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_path = pidfile_path
+        if self.pidfile_path is None:
+            self.pidfile_path = '/var/run/' + progname + '.pid'
+            
+        self.pidfile_timeout = pidfile_timeout
+
+    def run(self, daemon_mode=True):
+        if (daemon_mode):
+            logger.info('daemon starting, device=' + device 
+                        + ', filename=' + filename)
+
+        ser = Serial(device)
+        wait_signals = (TIOCM_RNG |
+                        TIOCM_DSR |
+                        TIOCM_CD  |
+                        TIOCM_CTS)
+
+        ser.setDTR(True)
+        file_exists = os.path.isfile(filename)
+        previous_file_exists = file_exists
+        ser.setDTR(not file_exists)
+
+        while True:
+            # ioctl(ser.fd, TIOCMIWAIT, TIOCM_CTS)
+            r = timeout(ioctl, [ser.fd, TIOCMIWAIT, TIOCM_CTS], 
+                        timeout_duration=1)
+
+            file_exists = os.path.isfile(filename)
+            ser.setDTR(not file_exists)
+
+            if file_exists != previous_file_exists:
+                # File existence has changed. 
+                if file_exists:
+                    logger.info(filename + ' created externally')
+                else:
+                    logger.info(filename + ' removed externally')
+                # Don't allow accept any switch presses for a second.
+                time.sleep(1)
+
+            elif r is not None:
+                # CTS changed state (or system call interrupted). Read
+                # CTS, sample 5 times in 250mS for switch debouncing
+                cts_count = 0
+                for i in range(5):
+                    time.sleep(0.050)
+                    if ser.getCTS():
+                        cts_count += 1
+
+                if cts_count >= 3:
+                    file_exists = not file_exists
+                    ser.setDTR(not file_exists)
+                    try:
+                        if file_exists:
+                            fh = open(filename, 'a')
+                            fh.close()
+                            logger.info(filename + ' created')
+                        else:
+                            os.remove(filename)
+                            logger.info(filename + ' removed')
+                        # Dont' allow state to be changed immediately
+                        time.sleep(1)
+                    except Exception as e:
+                        logger.error(str(e))
+                        for i in range(10):
+                            ser.setDTR(i % 2)
+                            time.sleep(0.2)
+
+            previous_file_exists = file_exists
+
+
+
+# Inspired by
+# http://stackoverflow.com/questions/492519/timeout-on-a-python-function-call
+def timeout(func, args=(), kwargs={}, timeout_duration=1, default=None):
+
+    class TimeoutError(Exception):
+        pass
+
+    def handler(signum, frame):
+        raise TimeoutError()
+
+    # set the timeout handler
+    signal.signal(signal.SIGALRM, handler) 
+    signal.alarm(timeout_duration)
+    try:
+        result = func(*args, **kwargs)
+    except TimeoutError as exc:
+        result = default
+    finally:
+        signal.alarm(0)
+
+    return result
+
+
+if __name__ == '__main__':
+    # Parse command line arguments
+    progname = os.path.basename(sys.argv[0]).partition('.')[0]
+    config_basename = re.sub('_monitor.*$', '', progname)
+    default_config_file = \
+        os.path.join(os.path.sep, 'etc', config_basename + '.ini')
+
+    parser = \
+        argparse.ArgumentParser(description='AuroraWatch data quality monitor')
+
+    parser.add_argument('-c', '--config-file', 
+                        default='/etc/awnet.ini',
+                        help='Configuration file')
+    parser.add_argument('--port', metavar='FILE', help='USB device')
+    parser.add_argument('--filename', metavar='FILE', help='Semaphore file')
+    parser.add_argument('--log-level', 
+                        choices=['debug', 'info', 'warning', 
+                                 'error', 'critical'],
+                        default='info',
+                        help='Control how much detail is printed',
+                        metavar='LEVEL')
+    parser.add_argument('--log-format',
+                        default='%(levelname)s:%(message)s',
+                        help='Set format of log messages',
+                        metavar='FORMAT')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-f', '--foreground', action='store_true',
+                       help='Run in foreground (not daemon)')
+
+    group.add_argument('action', nargs='?',
+                       choices=['start', 'stop', 'restart'],
+                       metavar='start|stop|restart',
+                       help='Daemon action')
+    args = parser.parse_args()
+    if not args.foreground and len(sys.argv) > 2:
+        # Fix sys.argv for the daemon to parse
+        sys.argv = [sys.argv[0], args.action]
+
+    config = awn.read_config_file(args.config_file)
+
+    logger.setLevel(getattr(logging, args.log_level.upper()))
+    log_formatter = logging.Formatter(args.log_format)
+    if config.has_option('dataqualitymonitor', 'logfile'):
+        log_filehandle = logging.FileHandler(config.get('dataqualitymonitor', 
+                                                        'logfile'))
+        log_filehandle.setFormatter(log_formatter)
+        logger.addHandler(log_filehandle)
+    else:
+        log_filehandle = None
+
+
+    device = args.port
+    if not device:
+        if config.has_option('dataqualitymonitor', 'port'):
+            device = config.get('dataqualitymonitor', 'port')
+        else:
+            raise Exception('FTDI device not specified')
+
+    filename = args.filename
+    if not filename:
+        if config.has_option('dataqualitymonitor', 'filename'):
+            filename = config.get('dataqualitymonitor', 'filename')
+        else:
+            raise Exception('Filename not specified')
+
+    if config.has_option('dataqualitymonitor', 'pidfile'):
+        pidfile_path = config.get('dataqualitymonitor', 'pidfile')
+    else:
+        pidfile_path = None
+    fm = FtdiMonitor(progname, device=device, filename=filename,
+                     pidfile_path=pidfile_path)
+
+    if args.foreground:
+        logger.info('running in foreground')
+        logger.info('device:' + fm.device)
+        logger.info('filename:' + fm.filename)
+        fm.run(False)
+    else:
+        
+        daemon_runner = daemon.runner.DaemonRunner(fm)
+        if log_filehandle:
+            daemon_runner.daemon_context.files_preserve=[log_filehandle.stream]
+        daemon_runner.do_action()
+
