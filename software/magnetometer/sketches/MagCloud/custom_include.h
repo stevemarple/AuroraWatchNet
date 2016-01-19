acfc68328f207db60dae0105998210f418217c33
Steve Marple <stevemarple@googlemail.com>
2016-01-19 23:12:07 +0000

    Include GNSS and memory usage when compiling from the IDE

--- software/magnetometer/sketches/MagCloud/custom_include.h
+++ software/magnetometer/sketches/MagCloud/custom_include.h
@@ -3,8 +3,12 @@
 
 // This file is used when compiling from the IDE to define
 // communication hardware support. It is not used by the Makefile.
-#define COMMS_XRF
-#define COMMS_W5100
+//#define COMMS_XRF
+//#define COMMS_W5100
+#define COMMS_W5500
 
+#define FEATURE_GNSS
+#define FEATURE_VERBOSITY 1
+#define FEATURE_MEM_USAGE
 
 #endif
