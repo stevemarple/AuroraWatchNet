e62b1c23e7651ebcdbce8376bf86c69b206b4cd2
Steve Marple <stevemarple@googlemail.com>
2017-11-05 23:06:21 +0000

    Fix for data quality directory and more
    Includes fix to use a data quality directory if one defined in ini file.
    Work around IO errors in an attempt to continue working (eg if serial
    device temporarily removed).

--- software/server/bin/ftdi_monitor.py
+++ software/server/bin/ftdi_monitor.py
@@ -14,19 +14,14 @@
 # to change the data quality status remotely.
 
 import argparse
+import logging
 import os
 import signal
 import sys
 import time
 from serial import Serial
-from fcntl import  ioctl
-from termios import (
-    TIOCMIWAIT,
-    TIOCM_RNG,
-    TIOCM_DSR,
-    TIOCM_CD,
-    TIOCM_CTS
-)
+from fcntl import ioctl
+import termios
 
 if sys.version_info[0] >= 3:
     import configparser
@@ -60,95 +55,143 @@ def timeout(func, args=(), kwargs={}, timeout_duration=1, default=None):
     return result
 
 
-# Parse command line arguments
-parser = \
-    argparse.ArgumentParser(description='AuroraWatch data recording daemon')
-
-parser.add_argument('-c', '--config-file', 
-                    default='/etc/awnet.ini',
-                    help='Configuration file')
-parser.add_argument('-d', '--daemon', action='store_true',
-                    help='Run as daemon')
-parser.add_argument('--port', metavar='FILE', help='USB device')
-parser.add_argument('--filename', metavar='FILE', help='Semaphore file')
-
-args = parser.parse_args()
-config = awn.read_config_file(args.config_file)
-
-device = args.port
-if not device:
-    if config.has_option('dataqualitymonitor', 'port'):
-        device = config.read('dataqualitymonitor', 'port')
-    else:
-        raise Exception('FTDI device not specified')
-
-filename = args.filename
-if not filename:
-    if config.has_option('dataqualitymonitor', 'filename'):
-        device = config.read('dataqualitymonitor', 'filename')
-    else:
-        raise Exception('Filename not specified')
-
-if args.daemon:
-    import daemon
-    pidfile = None
-    if config.has_option('dataqualitymonitor', 'pidfile'):
-        import lockfile
-        pidfile = lockfile.FileLock(config.get('dataqualitymonitor', 'pidfile'),
-                                    threaded=False)
-        if pidfile.is_locked():
-            print('daemon already running')
-            exit(1)
-    daemon.DaemonContext(pidfile=pidfile).open()
+def init_serial_device(device):
+    ser = Serial(device)
+    return ser
 
 
-ser = Serial(device)
-wait_signals = (TIOCM_RNG |
-                TIOCM_DSR |
-                TIOCM_CD  |
-                TIOCM_CTS)
-    
-ser.setDTR(True)
-file_exists = os.path.isfile(filename)
-previous_file_exists = file_exists
-ser.setDTR(not file_exists)
+if __name__ == '__main__':
 
-while True:
-    # ioctl(ser.fd, TIOCMIWAIT, TIOCM_CTS)
-    r = timeout(ioctl, [ser.fd, TIOCMIWAIT, TIOCM_CTS], timeout_duration=1)
+    logger = logging.getLogger(__name__)
 
+    # Parse command line arguments
+    default_config_filename = None
+    for f in ('/etc/ftdi_monitor.ini', '/etc/awnet.ini'):
+        if os.path.exists(f):
+            default_config_filename = f
+
+    parser = \
+        argparse.ArgumentParser(description='AuroraWatch data recording daemon')
+
+    # If an expected default does not exist then make it a required argument
+    parser.add_argument('-c', '--config-file',
+                        default=default_config_filename,
+                        required=False if default_config_filename else True,
+                        help='Configuration file')
+    parser.add_argument('-d', '--daemon', action='store_true',
+                        help='Run as daemon')
+    parser.add_argument('--log-level',
+                        choices=['debug', 'info', 'warning',
+                                 'error', 'critical'],
+                        default='info',
+                        help='Control how much detail is printed',
+                        metavar='LEVEL')
+    parser.add_argument('--port', metavar='FILE', help='USB device')
+    parser.add_argument('--filename', metavar='FILE', help='Semaphore file')
+
+    args = parser.parse_args()
+    logging.basicConfig(level=getattr(logging, args.log_level.upper()))
+
+    config = awn.read_config_file(args.config_file)
+
+    device = args.port
+    if not device:
+        if config.has_option('dataqualitymonitor', 'port'):
+            device = config.get('dataqualitymonitor', 'port')
+        else:
+            raise Exception('FTDI device not specified')
+
+    filename = args.filename
+    if not filename:
+        if config.has_option('dataqualitymonitor', 'filename'):
+            filename = config.get('dataqualitymonitor', 'filename')
+        elif config.has_option('dataqualitymonitor', 'directory'):
+            # Generate filename based on serial device in use
+            filename = os.path.join(config.get('dataqualitymonitor', 'directory'),
+                                    'ftdi_monitor' + '_' + os.path.basename(device))
+            logger.info('using %s as semaphore file', filename)
+        else:
+            raise Exception('Filename not specified')
+
+    if args.daemon:
+        import daemon
+        pidfile = None
+        if config.has_option('dataqualitymonitor', 'pidfile'):
+            import lockfile
+            pidfile = lockfile.FileLock(config.get('dataqualitymonitor', 'pidfile'),
+                                        threaded=False)
+            if pidfile.is_locked():
+                print('daemon already running')
+                exit(1)
+        daemon.DaemonContext(pidfile=pidfile).open()
+
+    ser = init_serial_device(device)
     file_exists = os.path.isfile(filename)
-    ser.setDTR(not file_exists)
-
-    if file_exists != previous_file_exists:
-        # File existence has changed. Don't allow accept any switch
-        # presses for a second.
-        time.sleep(1)
-
-    elif r is not None:
-        # CTS changed state (or system call interrupted). Read CTS, sample
-        # 5 times in 250mS for switch debouncing
-        cts_count = 0
-        for i in range(5):
-            time.sleep(0.050)
-            if ser.getCTS():
-                cts_count += 1
-
-        if cts_count >= 3:
-            file_exists = not file_exists
-            ser.setDTR(not file_exists)
-            try:
-                if file_exists:
-                    fh = open(filename, 'a')
-                    fh.close()
-                else:
-                    os.remove(filename)
-                # Dont' allow state to be changed immediately
-                time.sleep(1)
-            except Exception as e:
-                print(str(e))
-                for i in range(10):
-                    ser.setDTR(i % 2)
-                    time.sleep(0.2)
-            
     previous_file_exists = file_exists
+    ser.setDTR(file_exists)
+
+    while True:
+        try:
+            r = timeout(ioctl, [ser.fd, termios.TIOCMIWAIT, termios.TIOCM_CTS], timeout_duration=1)
+
+            file_exists = os.path.isfile(filename)
+            ser.setDTR(file_exists)
+
+            if file_exists != previous_file_exists:
+                # File existence has changed.
+                if file_exists:
+                    logger.info('state changed: %s created externally', filename)
+                else:
+                    logger.info('state changed: %s removed externally', filename)
+                # Don't accept any switch presses for a second.
+                time.sleep(1)
+
+            elif r is not None:
+                # CTS changed state (or system call interrupted). Read CTS, sample
+                # 5 times in 250ms for switch debouncing
+                cts_count = 0
+                for i in range(5):
+                    time.sleep(0.050)
+                    if ser.getCTS():
+                        cts_count += 1
+
+                if cts_count >= 3:
+                    # Toggle state
+                    file_exists = not file_exists
+                    ser.setDTR(file_exists)
+                    action = 'create/remove'
+                    try:
+                        if file_exists:
+                            action = 'create'
+                            fh = open(filename, 'a')
+                            fh.close()
+                        else:
+                            action = 'remove'
+                            os.remove(filename)
+                        # Don't allow state to be changed immediately
+                        logger.info('state changed: %s %sd due to button press', filename, action)
+                        time.sleep(1)
+                    except Exception as e:
+                        logger.exception('could not %s file %s', action, filename)
+                        print(str(e))
+                        for i in range(10):
+                            ser.setDTR(i % 2)
+                            time.sleep(0.2)
+                elif cts_count:
+                    logger.debug('button press failed debounce check')
+
+            previous_file_exists = file_exists
+
+        except IOError as e:
+            logger.exception('IO error')
+            while True:
+                # Try to resume (eg if device was unplugged)
+                try:
+                    ser.close()
+                    ser = init_serial_device(device)
+                    file_exists = os.path.isfile(filename)
+                    ser.setDTR(file_exists)
+                    logger.info('reopened device %s', device)
+                    break
+                except IOError:
+                    time.sleep(1)
