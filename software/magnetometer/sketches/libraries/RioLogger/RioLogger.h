1d2d1d073bc01cc73fd19ed4678b802355e7e5ce
Steve Marple <steve@stevemarple.co.uk>
2018-04-24 12:00:46 +0100

    Add support for riometer logger hardware
    Software support based on FLC100 shield software sa the hardware is very
    similar.

---
+++ software/magnetometer/sketches/libraries/RioLogger/RioLogger.h
@@ -0,0 +1,139 @@
+#ifndef RIO_LOGGER_H
+#define RIO_LOGGER_H
+
+#include <stdint.h>
+#include <AsyncDelay.h>
+#include <MCP342x.h>
+#include <CounterRTC.h>
+
+#define FLC100_POWER 9
+#define FLC100_TEMPERATURE_CHANNEL 4
+
+
+class RioLogger;
+
+
+class RioLogger {
+public:
+  static const uint8_t numAxes = 3;
+  static const unsigned long defaultPowerUpDelay_ms = 1000;
+  static unsigned long powerUpDelay_ms;
+  static const uint8_t maxSamples = 16;
+
+  RioLogger(void);
+
+  inline bool isFinished(void) const {
+      return state == finished;
+    }
+
+  inline bool isSampling(void) const {
+    return !(state == off || state == finished);
+    }
+
+  inline void start(void) {
+    state = poweringUp;
+    digitalWrite(powerPin, HIGH);
+    delay.start(powerUpDelay_ms, AsyncDelay::MILLIS);
+  }
+
+
+  inline CounterRTC::time_t getTimestamp(void) const {
+    return timestamp;
+  }
+
+  inline int16_t getSensorTemperature(void) const {
+    return sensorTemperature;
+  }
+
+  inline bool getAdcPresent(uint8_t n) const {
+      if (n >= numAxes)
+	return false;
+      return adcPresent[n];
+  }
+
+  inline const int32_t* getData(void) const {
+    return magData;
+  }
+
+  inline const int32_t* getDataSamples(uint8_t mag) const {
+    return magDataSamples[mag];
+  }
+
+  inline int32_t getDataSamples(uint8_t mag, uint8_t sampleNum) const {
+    return magDataSamples[mag][sampleNum];
+  }
+
+  inline uint8_t getResGain(uint8_t mag) const {
+    return (mag < numAxes) ? (adcConfig[mag] & 0x0F) : 0;
+  }
+
+  inline int getState(void) const {
+    return state;
+  }
+
+  inline void getNumSamples(uint8_t &numSamp, bool &useMed,
+			      bool &trimSamp) const {
+    numSamp = numSamples;
+    useMed = useMedian;
+    trimSamp = trimSamples;
+  }
+
+  inline void setNumSamples(uint8_t numSamp, bool useMed, bool trimSamp) {
+    if (numSamp > 0 && numSamp <= maxSamples) {
+      numSamples = numSamp;
+      useMedian = useMed;
+      trimSamples = trimSamp;
+    }
+  }
+
+
+  bool initialise(uint8_t pp, uint8_t adcAddressList[numAxes],
+		  uint8_t adcChannelList[numAxes],
+		  uint8_t adcResolutionList[numAxes],
+		  uint8_t adcGainList[numAxes]);
+  void process(void);
+  void finish(void);
+
+private:
+
+  enum state_t {
+    off,
+    poweringUp,
+    readingTime,
+    convertingTemp,
+    readingTemp,
+    configuringRioADCs,
+    convertingRioADCs,
+    readingRioADCs,
+    poweringDown,
+    finished,
+  };
+
+  state_t state;
+  uint8_t axis;
+  uint8_t powerPin;
+
+  MCP342x adc[numAxes]; // X, Y, Z
+  MCP342x::Config adcConfig[numAxes];
+  MCP342x::Config tempConfig;
+  bool adcPresent[numAxes];
+
+  AsyncDelay delay;
+  AsyncDelay timeout;
+
+  // Data fields
+  CounterRTC::time_t timestamp;
+  int16_t sensorTemperature; // hundredths of degrees Celsius
+  int32_t magData[numAxes];  // averaged from a number of samples
+  int32_t magDataSamples[numAxes][maxSamples]; // individual results
+
+  uint8_t numSamples;
+  bool useMedian;
+  bool trimSamples;
+
+  void aggregate(void);
+
+};
+
+
+#endif
