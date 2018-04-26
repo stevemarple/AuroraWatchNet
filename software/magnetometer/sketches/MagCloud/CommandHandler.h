551ff16132285325f813a6940a1b23431783d84e
Steve Marple <stevemarple@googlemail.com>
2018-04-26 21:36:19 +0100

    Fix indents

--- software/magnetometer/sketches/MagCloud/CommandHandler.h
+++ software/magnetometer/sketches/MagCloud/CommandHandler.h
@@ -9,23 +9,23 @@
 
 class CommandHandler {
 public:
-  // Return clock error
-  static bool checkTime(RTCx::time_t t, RTCx::time_t &err);
-  static bool setTime(RTCx::time_t t);
-  
-  inline CommandHandler(void);
-  
-  void process(Stream &console);
-  
+	// Return clock error
+	static bool checkTime(RTCx::time_t t, RTCx::time_t &err);
+	static bool setTime(RTCx::time_t t);
+
+	inline CommandHandler(void);
+
+	void process(Stream &console);
+
 private:
-  static const uint8_t bufferLen = 80;
-  char buffer[bufferLen];
-  char *ptr;
+	static const uint8_t bufferLen = 80;
+	char buffer[bufferLen];
+	char *ptr;
 };
 
 CommandHandler::CommandHandler(void) : ptr(buffer)
 {
-  ;
+	;
 }
 
 
