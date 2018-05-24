cbb965c07cae57a2698006d49b15991424b384ad
Steve Marple <stevemarple@googlemail.com>
2018-05-24 13:37:28 +0100

    Add support for ATmega1284 (non-P vdrsion)

--- software/magnetometer/sketches/libraries/Ethernet2/src/utility/w5500.h
+++ software/magnetometer/sketches/libraries/Ethernet2/src/utility/w5500.h
@@ -331,7 +331,7 @@ private:
 
 private:
 #if defined(ARDUINO_ARCH_AVR)
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284P__)
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega1284__)
   inline static void initSS()    { DDRB  |=  _BV(4); };
   inline static void setSS()     { PORTB &= ~_BV(4); };
   inline static void resetSS()   { PORTB |=  _BV(4); };
