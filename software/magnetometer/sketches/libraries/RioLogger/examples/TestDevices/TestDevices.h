fc31a37e356d7c7460261bde8f9d60b87fe0db2d
Steve Marple <steve@stevemarple.co.uk>
2018-09-13 11:15:07 +0100

    Add missing header file

---
+++ software/magnetometer/sketches/libraries/RioLogger/examples/TestDevices/TestDevices.h
@@ -0,0 +1,37 @@
+#ifndef TEST_DEVICES_H
+#define TEST_DEVICES_H
+
+#define EXPAND_STR(s) _MY_EXPAND_STR(s)
+#define _MY_EXPAND_STR(s) #s
+
+#if F_CPU == 8000000UL
+#define F_CPU_STR "8MHz"
+#elif F_CPU == 12000000UL
+#define F_CPU_STR "12MHz"
+#elif F_CPU == 16000000UL
+#define F_CPU_STR "16MHz"
+#elif F_CPU == 20000000UL
+#define F_CPU_STR "20MHz"
+#else
+#error Unknown F_CPU value
+#endif
+
+// Signatures to identify actual MCU fitted
+#ifdef __AVR__
+#define DEVICE_SIG_ATMEGA1284P 0x1e9705
+#define DEVICE_SIG_ATMEGA1284 0x1e9706
+#define DEVICE_SIG_ATMEGA644P 0x1e960a
+#define DEVICE_SIG_ATMEGA644  0x1e9609
+#endif
+
+#ifndef CPU_NAME
+#ifdef __AVR__
+#define CPU_NAME __AVR_DEVICE_NAME__
+#else
+#error Please define CPU_NAME
+#endif
+#endif
+
+const char* createDeviceName(uint32_t deviceSignature);
+
+#endif
