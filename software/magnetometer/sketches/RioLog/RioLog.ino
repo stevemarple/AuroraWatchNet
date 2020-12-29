b6ac77bfe1497a07354ebba314f1bc4f4fba2075
Steve Marple <s.marple@systemloco.com>
2020-12-29 16:40:43 +0000

    Bump version: 1.2g → 1.2h

--- software/magnetometer/sketches/RioLog/RioLog.ino
+++ software/magnetometer/sketches/RioLog/RioLog.ino
@@ -171,7 +171,7 @@
 #define STRINGIFY(a) STRINGIFY2(a)
 #define STRINGIFY2(a) #a
 
-#define FIRMWARE_VERSION "RioLog-1.2g"
+#define FIRMWARE_VERSION "RioLog-1.2h"
 //                        1234567890123456
 // Firmware version limited to 16 characters
 
