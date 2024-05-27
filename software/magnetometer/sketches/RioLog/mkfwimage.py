05a28da52cf8b57a183e7e094814f1cbb8ecab05
Steve Marple <stevemarple@googlemail.com>
2024-05-27 12:16:56 +0100

    [RioLog] Update mkfwimage.py for Python 3

--- software/magnetometer/sketches/RioLog/mkfwimage.py
+++ software/magnetometer/sketches/RioLog/mkfwimage.py
@@ -50,16 +50,16 @@ bin_contents = bin_file.read()
 block_size = awn.message.firmware_block_size
 if len(bin_contents) % block_size:
     # Pad the file to the block size used for transmission
-    padding = chr(0xFF) * (block_size - (len(bin_contents) % block_size))
+    padding = b'\xff' * (block_size - (len(bin_contents) % block_size))
     bin_contents += padding
     bin_file.write(padding)
 bin_file.close()
 
 # The CRC check must be computed over the entire temporary 
 # application section; extend as necessary
-temp_app_size = (131072 - 4096) / 2;
+temp_app_size = (131072 - 4096) // 2;
 if len(bin_contents) < temp_app_size:
-    padding = chr(0xFF) * (temp_app_size - len(bin_contents))
+    padding = b'\xff' * (temp_app_size - len(bin_contents))
     bin_contents += padding
 elif len(bin_contents) > temp_app_size:
     print('Firmware image too large (' + str(len(bin_contents)) + ' bytes)')
@@ -69,7 +69,7 @@ crc = awn.message.crc16(bin_contents)
 crc_file = open(crc_filename, 'w')
 crc_str = struct.pack('>H', crc)
 # Output in similar way to md5sum
-print(binascii.hexlify(crc_str) + '  ' + options.firmware_version, 
+print(binascii.hexlify(crc_str).decode('ascii') + '  ' + options.firmware_version,
       file=crc_file)
 crc_file.close()
 
